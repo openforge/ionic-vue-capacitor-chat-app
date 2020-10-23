86dcde62465b67792919dfc9b6f486949e610b43
daftclaud <cdsmileyone@gmail.com>
2020-10-23 12:00:24 -0400

    imports ion-icon

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,68 +1,39 @@
 <template>
   <ion-page>
-    <ion-header :translucent="true">
-      <ion-toolbar>
-        <ion-title>Blank</ion-title>
-      </ion-toolbar>
-    </ion-header>
-    
-    <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Blank</ion-title>
-        </ion-toolbar>
-      </ion-header>
-    
-      <div id="container">
-        <strong>Ready to create an app?</strong>
-        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
-      </div>
-    </ion-content>
+    <ion-content :fullscreen="true"> </ion-content>
+    <ion-footer class="ion-padding">
+      <ion-input />
+      <ion-icon :icon="send"></ion-icon>
+    </ion-footer>
   </ion-page>
 </template>
 
 <script lang="ts">
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
-import { defineComponent } from 'vue';
+import { send } from "ionicons/icons";
+import { IonContent, IonPage, IonFooter, IonInput } from "@ionic/vue";
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: 'Home',
+  name: "Home",
   components: {
     IonContent,
-    IonHeader,
     IonPage,
-    IonTitle,
-    IonToolbar
-  }
+    IonFooter,
+    IonInput,
+  },
+  setup() {
+    return { send };
+  },
 });
 </script>
 
 <style scoped>
-#container {
-  text-align: center;
-  
-  position: absolute;
-  left: 0;
-  right: 0;
-  top: 50%;
-  transform: translateY(-50%);
+ion-footer {
+  background: var(--ion-color-medium);
 }
-
-#container strong {
-  font-size: 20px;
-  line-height: 26px;
-}
-
-#container p {
-  font-size: 16px;
-  line-height: 22px;
-  
-  color: #8c8c8c;
-  
-  margin: 0;
-}
-
-#container a {
-  text-decoration: none;
+ion-input {
+  --background: var(--ion-color-medium-contrast);
+  border-radius: 20px;
+  --padding-start: 15px;
 }
 </style>
