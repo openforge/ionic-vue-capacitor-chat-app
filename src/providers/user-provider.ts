f5d97ecbf8caf582a82b55265bc080f69f54b6a1
daftclaud <cdsmileyone@gmail.com>
2020-10-26 23:04:36 -0400

    adds chat list

--- src/providers/user-provider.ts
+++ src/providers/user-provider.ts
@@ -3,30 +3,42 @@ import { computed, ComputedRef, reactive, readonly } from 'vue';
 export interface User {
   id: string;
   name: string;
+  chatIDs: string[];
 }
 
 export interface UserProvider {
   state: {
     id: string;
     name: string;
+    chatIDs: string[];
   };
   setUser: (user: User) => void;
+  addChatID: (id: string) => void;
   name: ComputedRef<string>;
+  chatIDs: ComputedRef<string[]>;
 }
 
 const state = reactive<User>({
   id: '',
   name: '',
+  chatIDs: [],
 });
 
 const setUser = (user: User) => {
   Object.assign(state, user);
 };
 
+const addChatID = (id: string) => {
+  state.chatIDs.push(id);
+}
+
 const name = computed(() => state.name);
+const chatIDs = computed(() => state.chatIDs);
 
 export const userStore = readonly({
   state,
   setUser,
+  addChatID,
   name,
+  chatIDs,
 });
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,23 +2,39 @@
   <ion-page>
     <ion-content :fullscreen="true" class="ion-padding ion-text-center">
       <h1>Welcome {{ name }}</h1>
+      <ion-button
+        class="ion-margin"
+        color="success"
+        @click.prevent="createChat()"
+        >Add New Chat</ion-button
+      >
+      <ion-list>
+        <ion-item></ion-item>
+      </ion-list>
     </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
 import { computed, defineComponent, inject } from "vue";
-import { IonContent, IonPage } from "@ionic/vue";
+import { IonContent, IonPage, IonList, IonItem } from "@ionic/vue";
 import { UserProvider } from "@/providers/user-provider";
-
+import { chatCollection } from "../firebase";
 export default defineComponent({
   name: "Home",
-  components: { IonPage, IonContent },
+  components: { IonPage, IonContent, IonList, IonItem },
   setup() {
     const userStore = inject<UserProvider>("userStore");
     const name = computed(() => userStore?.name);
+    const chatIDs = computed(() => userStore?.chatIDs);
 
-    return { name };
+    function createChat() {
+      chatCollection.add({ messages: [] }).then((ref) => {
+        ref.set({ id: ref.id }, { merge: true });
+      });
+    }
+
+    return { name, createChat, chatIDs };
   },
 });
 </script>
