0b3f0fb6c3301e1505e31040e136dbee9634894e
daftclaud <cdsmileyone@gmail.com>
2020-10-26 22:43:14 -0400

    adds chat provider

---
+++ src/providers/chats-provider.ts
@@ -0,0 +1,48 @@
+import { computed, ComputedRef, reactive, readonly } from 'vue';
+import { User } from './user-provider';
+
+export interface Chat {
+    id: string;
+    messages: {
+        body: string;
+        sender: User;
+    }[];
+}
+
+export interface ChatProvider {
+    state: {
+        id: string;
+        messages: {
+            body: string;
+            sender: {
+                id: string;
+                name: string;
+            };
+        }[];
+    };
+    setChat: (chat: Chat) => void;
+    messages: ComputedRef<{
+        body: string;
+        sender: {
+            id: string;
+            name: string;
+        };
+    }[]>;
+}
+
+const state = reactive<Chat>({
+    id: '',
+    messages: [],
+});
+
+const setChat = (chat: Chat) => {
+    Object.assign(state, chat);
+};
+
+const messages = computed(() => state.messages);
+
+export const chatStore = readonly({
+    state,
+    setChat,
+    messages,
+})
