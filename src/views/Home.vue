f2b0e9aea7473716aedb30c19e15f4a12d05a52a
daftclaud <cdsmileyone@gmail.com>
2020-10-26 21:14:32 -0400

    shows error state

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,12 +8,21 @@
         <ion-input type="email" placeholder="Email" v-model="email" />
         <ion-input type="password" placeholder="Password" v-model="password" />
 
+        <ion-text color="danger" v-if="error">{{error}}</ion-text>
+
         <ion-button
-          @click.prevent="login()"
+          @click.prevent="logIn()"
           class="ion-margin"
           color="light"
           expand="block"
           >login</ion-button
+        >
+        <ion-button
+          @click.prevent="signUp()"
+          class="ion-margin"
+          color="medium"
+          expand="block"
+          >signup</ion-button
         >
       </div>
     </ion-content>
@@ -21,9 +30,10 @@
 </template>
 
 <script lang="ts">
-import { IonContent, IonPage, IonInput, IonButton } from "@ionic/vue";
+import { IonContent, IonPage, IonInput, IonButton, IonText } from "@ionic/vue";
+import { auth as Auth } from "firebase";
 import { defineComponent, reactive, toRefs } from "vue";
-import { auth } from "../firebase";
+import { auth, userCollection } from "../firebase";
 
 export default defineComponent({
   name: "Home",
@@ -32,17 +42,52 @@ export default defineComponent({
     IonPage,
     IonInput,
     IonButton,
+    IonText
   },
   setup() {
     const formData = reactive({
       name: "",
       email: "",
       password: "",
+      error: "",
     });
-    function login() {
-      return auth.signInWithEmailAndPassword(formData.email, formData.password);
+
+    function updateUser(auth: Auth.UserCredential) {
+      if (!auth.user) return;
+
+      const {
+        user: { uid },
+      } = auth;
+
+      const user = {
+        id: uid,
+        name: formData.name,
+      };
+
+      userCollection.doc(uid).update(user);
     }
-    return { ...toRefs(formData), login };
+
+    function logIn() {
+      return auth
+        .signInWithEmailAndPassword(formData.email, formData.password)
+        .then(updateUser)
+        .catch(e => {
+          console.warn(e);
+          formData.error = "User does not exist"
+        });
+    }
+
+    function signUp() {
+      return auth
+        .createUserWithEmailAndPassword(formData.email, formData.password)
+        .then(updateUser)
+        .catch(e => {
+          console.warn(e);
+          formData.error = "User already exists"
+        });
+    }
+
+    return { ...toRefs(formData), logIn, signUp };
   },
 });
 </script>
