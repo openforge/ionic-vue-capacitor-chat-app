4c82fd681b58f17a5158ccb7b76891100f6248c6
daftclaud <cdsmileyone@gmail.com>
2020-10-26 21:22:48 -0400

    create home page

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,12 +1,18 @@
 import { createRouter, createWebHistory } from '@ionic/vue-router';
 import { RouteRecordRaw } from 'vue-router';
 import Auth from '../views/Auth.vue';
-import Chat from '../views/Chat.vue'
+import Chat from '../views/Chat.vue';
+import Home from '../views/Home.vue';
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     redirect: '/auth'
+  },
+  {
+    path: '/home',
+    name: 'Home',
+    component: Home
   },
   {
     path: '/auth',
---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <ion-page>
+      <ion-content :fullscreen="true" class="ion-padding ion-text-center">
+          <h1>Welcom User</h1>
+      </ion-content>
+  </ion-page>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { IonContent, IonPage } from "@ionic/vue";
+export default defineComponent({
+  name: "Home",
+  components: { IonPage, IonContent },
+});
+</script>
